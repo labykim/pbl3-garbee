4497e5b545ec4711e829da54be65001f39591c4d
labykim <fingskim0229@gmail.com>
2022-06-30 22:34:07 +0900

    Merge branch 'dev' of https://github.com/labykim/pbl3-garbee into artie/dev_error-handling

--- lib/classifier.dart
+++ lib/classifier.dart
@@ -1,5 +1,28 @@
-String classifier(List<String> input) {
-  String output = '';
+dynamic classifier(List<String> input) {
+  List<String> output = [];
+
+
+
+  Map<String, String> findCategory = {
+    "Plastic bottle" : "PET",
+    "Paper cup" : "Combustible",
+    "Instant noodle" : "Combustible",
+    "Pen" : "Plastics",
+    "Packaged goods" : "",
+    "Tumbler" : "",
+    "Bottle" : "null",
+  };
+
+  if(output == "null") {
+    return null;
+  }
+
+  Map<String, List<String>> findInstruction = {
+    "Plastics" : [""],
+    "PET" : ["1. Separate the cap and the label.", "2. Bottle to PET, cap and label to plastics."],
+  };
+
+
 
   return output;
 }
