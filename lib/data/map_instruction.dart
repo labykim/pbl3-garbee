7f7c2bcc8ddc546de7828093024b8eb7dc79763f
labykim <fingskim0229@gmail.com>
2022-07-08 17:09:55 +0900

    Update DB

--- lib/data/map_instruction.dart
+++ lib/data/map_instruction.dart
@@ -1,23 +1,28 @@
 Map<String, List<String>> findInstruction = {
-  'Combustible' : 
-    ['Just throw away'],
-  'Plastics' :
-    ['Wash it'],
-  'PET' : [
-    '1. Separate the cap and the label.',
-    '2. Wash the bottle inside, and dispose as PET.',
+  '燃やせるゴミ (Combustible)' : [
+    'Remember to remove the incombustible parts.'
+  ],
+  '燃やせないゴミ (Incombustible)' : [
+    'Please clean up before throwing it away.'
+  ],
+  'プラスチック製容器包装 (PET)' : [
+    '1. Separate the cap and the label.' ,
+    '2. Wash the bottle inside, and dispose as PET.' ,
     '3. Dispose the others as Plastics.'
   ],
-  'Glass': [
+  'Plastics' : [
+    'Wash it'
+  ],
+  'Glass' : [
     '1. Wash inside.',
     '2. Find a container for glass bottle and throw away.',
   ],
-  'Flower': [
+  'Flower' : [
     'inst. line 1',
     'inst. line 2',
     'inst. line 3',
   ],
-  'FAQ': [
+  'FAQ' : [
     '1. It is not Frequently Asked Questions.',
     '2. Pronounce it.',
   ]
